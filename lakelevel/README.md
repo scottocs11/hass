6112e17ef9a660d43c6b099101c7e8e64cdf84b8
Scott Griffin <scottgriffin08@gmail.com>
2026-05-25 01:21:27 -0400

    Basically v2. Changed from Chickamauga lake to all lakes, using dropdown menu

--- lakelevel/README.md
+++ lakelevel/README.md
@@ -1,20 +1,21 @@
 This script updates four HASS sensors to show the Chickamauga Lake level.
 
-![Screenshot of the completed product in HASS](https://github.com/scottocs11/hass/blob/main/lakelevel/images/chiclake.png)
+![Screenshot of the completed product in HASS](https://github.com/scottocs11/hass/blob/main/lakelevel/images/lakelevelcard.png)
+
+Prerequisites:
+1. Download and install [HACS](https://github.com/hacs). Restart HASS.
+2. Using HACS, download [Pyscript](https://github.com/custom-components/pyscript). Restart HASS.
 
 Instructions:
-1. Create four Helper Template Sensors via Settings -> Helpers -> Create Helper -> Template -> Sensor.
+1. Download lakelevel.py and copy it to your <HASSconfig>/pyscript folder.
+2. Reload Pyscript. The four sensors should now have the new values.
+3. Add a new card to your dash using the YAML from [card_config.yaml](https://github.com/scottocs11/hass/blob/main/lakelevel/card_config.yaml)
+4. Add a new automation using the YAML from [notifications.yaml](https://github.com/scottocs11/hass/blob/main/lakelevel/notifications.yaml).
+8.
 
-| Name | State | Unit of Measurement | State Class |
-| :--- | :--- | :--- | :--- |
-| Chickamauga Lake Full | {{0}} | ft | Measurement |
-| Chickamauga Lake Level | {{0}} | ft | Measurement |
-| Chickamauga Level Difference | {{0}} | ft | Measurement |
-| Chickamauga Refresh Time | {{0}} |  |  |
+Known Bugs:
+1. Hass' INPUT SELECT removes one of the two required spaces between the lake name and the state.
+2. Unique ID's are not set for sensors.
 
-2. Download and install [HACS](https://github.com/hacs) if not already installed. Restart HASS.
-3. Using HACS, download [Pyscript](https://github.com/custom-components/pyscript). Restart HASS.
-4. Download lakelevel.py and copy it to your <HASSconfig>/pyscript folder.
-5. Reload Pyscript
-6. The four sensors should now have the new values.
-7. Add a new automation using the YAML from [notifications.yaml](https://github.com/scottocs11/hass/blob/main/lakelevel/notifications.yaml).
+To Do:
+1. Create and set the dropdown menu with all state names, or text box to type the name. Unfortunately, HASS doesn't have a way to autocomplete the lake name.
--- lakelevel/lakelevel.py
+++ lakelevel/lakelevel.py
@@ -1,22 +1,98 @@
 import requests
 import pandas as pd
 from io import StringIO
-url = "https://www.lakelevels.info/USA/Tennessee/"
+url = "https://www.lakelevels.info/"
+# html = requests.get(url)
+# lakename = "Chickamauga  (TN)"
 html = await hass.async_add_executor_job(requests.get, url)
 df_list = pd.read_html(StringIO(html.text))
-df = df_list[3]
-levelCurrent = df.at[3,'Current Level']
-levelCurrent = levelCurrent.item()
-levelFull = df.at[3,'Full Pool']
-levelFull = levelFull.item()
-levelDifference = df.at[3,'+/- Full Pool']
-levelDifference = levelDifference.item()
-levelDate = df.at[3,'Reading Date - Time']
+df = df_list[4]
 
-@time_trigger("startup", "once(06:00:00)")
 def update_lake_info():
-    val = print(levelCurrent)
-    state.set("sensor.chickamauga_lake_full", value=levelFull, attr={'unit_of_measurement': 'ft'})
-    state.set("sensor.chickamauga_lake_level", value=levelCurrent, attr={'unit_of_measurement': 'ft'})
-    state.set("sensor.chickamauga_level_difference", value=levelDifference, attr={'unit_of_measurement': 'ft'})
-    state.set("sensor.chickamauga_refresh_time", value=levelDate)
+    html = await hass.async_add_executor_job(requests.get, url)
+    df_list = pd.read_html(StringIO(html.text))
+    df = df_list[4]
+    lakename = state.get("input_select.lake")
+    fdf = df[df['Lake Name'].str.contains(lakename)].reset_index()
+    levelCurrent = fdf.at[0,'Current Level']
+    levelCurrent = levelCurrent.item()
+    levelFull = fdf.at[0,'Full Pool']
+    levelFull = levelFull.item()
+    levelDifference = fdf.at[0,'+/- Full Pool']
+    levelDifference = levelDifference.item()
+    levelDate = fdf.at[0,'Reading Date - Time']
+
+    state.set('sensor.lake_current_level', levelCurrent, {
+    'friendly_name': 'Current Level',
+    'unit_of_measurement': 'ft',
+    'state class': 'measurement',
+    'icon': 'mdi:waves'
+    })
+
+    state.set('sensor.lake_full_pool', levelFull, {
+    'friendly_name': 'Full Pool',
+    'unit_of_measurement': 'ft',
+    'state class': 'measurement',
+    'icon': 'mdi:moon-full'
+    })
+
+    state.set('sensor.lake_level_difference', levelDifference, {
+    'friendly_name': 'Difference',
+    'unit_of_measurement': 'ft',
+    'state class': 'measurement',
+    'icon': 'mdi:waves-arrow-up'
+    })
+
+    state.set('sensor.lake_last_update', levelDate, {
+    'friendly_name': 'Last Update',
+    'icon': 'mdi:update'
+    })
+
+def quick_update_lake_info():
+    lakename = state.get("input_select.lake")
+    fdf = df[df['Lake Name'].str.contains(lakename)].reset_index()
+    levelCurrent = fdf.at[0,'Current Level']
+    levelCurrent = levelCurrent.item()
+    levelFull = fdf.at[0,'Full Pool']
+    levelFull = levelFull.item()
+    levelDifference = fdf.at[0,'+/- Full Pool']
+    levelDifference = levelDifference.item()
+    levelDate = fdf.at[0,'Reading Date - Time']
+
+    state.set('sensor.lake_current_level', levelCurrent, {
+    'friendly_name': 'Current Level',
+    'unit_of_measurement': 'ft',
+    'state class': 'measurement',
+    'icon': 'mdi:waves'
+    })
+
+    state.set('sensor.lake_full_pool', levelFull, {
+    'friendly_name': 'Full Pool',
+    'unit_of_measurement': 'ft',
+    'state class': 'measurement',
+    'icon': 'mdi:moon-full'
+    })
+
+    state.set('sensor.lake_level_difference', levelDifference, {
+    'friendly_name': 'Difference',
+    'unit_of_measurement': 'ft',
+    'state class': 'measurement',
+    'icon': 'mdi:waves-arrow-up'
+    })
+
+    state.set('sensor.lake_last_update', levelDate, {
+    'friendly_name': 'Last Update',
+    'icon': 'mdi:update'
+    })
+
+@time_trigger("once(06:00:00)")
+def update_sensors_time():
+    update_lake_info()
+
+@time_trigger("once(now)","startup")
+def update_sensors_time():
+    quick_update_lake_info()
+
+@state_trigger("input_select.lake")
+def update_sensors_lakechange():
+    quick_update_lake_info()
